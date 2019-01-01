a1b6215277dacb4d90153a304eeb4bb701560786
Sarfraj <breezy.safu@gmail.com>
2019-01-01 21:13:21 +0545

    3

---
+++ resources/views/inc/sideArea.blade.php
@@ -0,0 +1,12 @@
+<!-- Side Area -->
+<div id="side-area-main" class="col-md-2">
+    Menu
+    <div id="side-area-container">
+        <ul id="side-list">
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Add New Post</a></li>
+            <li><a href="#">Categories</a></li>
+            <li><a href="#">Admin</a></li>
+        </ul>
+    </div>
+</div>
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,50 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}"></script>
-    <title>Document</title>
+@extends('layouts.app')
 
-    <style>
-        #side-area-main {
-            background-color: rgb(57,208,129);
-            min-height:500px;
-        }
-
-        #side-list {
-            list-style:none;
-            color:#fff;
-            font-size:20px;
-        }
-    </style>
-
-</head>
-<body>
-
-<div class="container-fluid">
-    <div class="row">
-
-        <!-- Side Area -->
-        <div id="side-area-main" class="col-md-2">
-            Ipenywis
-            <div id="side-area-container">
-                <ul id="side-list">
-                    <li><a href="#">Dashboard</a></li>
-                    <li><a href="#">Add New Post</a></li>
-                    <li><a href="#">Categories</a></li>
-                    <li><a href="#">Admin</a></li>
-                </ul>
-            </div>
-        </div>
-
-        <!-- Main Area -->
-
-    </div>
-</div>
-
-</body>
-</html>
+@section('main-row')
+    @include('inc.sideArea')
+    <!-- Main Area -->
+@endsection
